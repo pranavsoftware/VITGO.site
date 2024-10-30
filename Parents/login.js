0306e5c7413496fe79bb255461838c84e74ee22c
Pranav Rayban <raybanpranav.mahesh2023@vitstudent.ac.in>
2024-10-30 20:33:43 +0530

    Update login.js

--- Parents/login.js
+++ Parents/login.js
@@ -123,13 +123,3 @@ document.getElementById("registerForm").addEventListener("submit", async (e) => 
         }
     }
 });
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../Parents/service-worker.js')
-        .then((registration) => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch((error) => {
-            console.log('Service Worker registration failed:', error);
-        });
-}
