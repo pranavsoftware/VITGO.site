94afe0c25b9d32a53bec94acb54545a5aa154bce
Pranav Rayban <raybanpranav.mahesh2023@vitstudent.ac.in>
2024-11-06 21:58:31 +0530

    Update About Us.js

--- About Us/About Us.js
+++ About Us/About Us.js
@@ -2,3 +2,10 @@ function toggleMenu() {
     const menuList = document.querySelector('.menu-list');
     menuList.classList.toggle('show');
 }
+// Function to hide the spinner after the page is fully loaded
+window.addEventListener('load', function() {
+    const spinner = document.getElementById('spinner');
+    if (spinner) {
+        spinner.style.display = 'none'; // Hides the spinner
+    }
+});
